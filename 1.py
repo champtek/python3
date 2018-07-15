4fd9f4b92639f635908c6890a503009ff3758194
champ <6984680@qq.com>
2018-07-15 15:47:47 +0800

    first

---
+++ 1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import xlrd
+import xlwt
+
+
+import json
+from urllib.request import urlopen
+url = "https://gdata.youtube.com/feeds/api.standardfeeds/top_rated?alt=json"
+response = urlopen(url)
+contents = response.read()
+text = contents.decode('utf8')
+data = json.load(text)
+for video in data['feed']['entry'][0:6]:
+    print(video['title']['$t'])
